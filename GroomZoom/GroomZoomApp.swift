4753b24561618d7b9e3ea5102e1459b55be30561
dljardim <dljardim@outlook.com>
2025-04-19 14:04:44 -0500

    Initial Commit

---
+++ GroomZoom/GroomZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroomZoomApp.swift
+//  GroomZoom
+//
+//  Created by Damian Jardim on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct GroomZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
